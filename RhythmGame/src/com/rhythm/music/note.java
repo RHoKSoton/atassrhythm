28df258218a95c64fe16abbc9dbf92954f2d6e17
Alex Lay <electrical34@gmail.com>
2012-12-01 14:47:28 +0000

    some note stuff added

--- RhythmGame/src/com/rhythm/music/note.java
+++ RhythmGame/src/com/rhythm/music/note.java
@@ -1,5 +1,31 @@
 package com.rhythm.music;
 
+import android.graphics.Bitmap;
+
 public class Note {
 
+	protected double trueLength;
+	protected double undottedLength;
+	protected Bitmap icon;
+	
+	public Note(double length, int dots)
+	{
+		undottedLength = length;
+		trueLength = length * (2-2^(-dots));
+	}
+	
+	protected Bitmap getIcon(int length){
+		switch(length){
+		case 1 : 
+			
+		case 0.5 :
+		
+		case 0.25 :
+			
+			
+		}
+		return null;
+	}
+	
+	
 }
