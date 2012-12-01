c36c891fb491a74ab5c95232fd4fe2f9c3de19b8
Alex Lay <electrical34@gmail.com>
2012-12-01 17:10:08 +0000

    actually updated constructors

--- RhythmGame/src/com/rhythm/music/Minim.java
+++ RhythmGame/src/com/rhythm/music/Minim.java
@@ -5,7 +5,7 @@ import com.rhythm.app.MainActivity;
 public class Minim extends Note 
 {
 
-	protected Minim(int dots) 
+	public Minim(int dots) 
 	{
 		super(0.5, dots, MainActivity.getBitmap("two_rest"), MainActivity.getBitmap("two_single"));
 		// TODO Auto-generated constructor stub
--- RhythmGame/src/com/rhythm/music/Semibreve.java
+++ RhythmGame/src/com/rhythm/music/Semibreve.java
@@ -6,7 +6,7 @@ public class Semibreve extends Note
 {
 
 	
-	protected Semibreve(int dots) 
+	public Semibreve(int dots) 
 	{
 		super(1, dots, MainActivity.getBitmap("one_rest"), MainActivity.getBitmap("one_single"));
 	}
