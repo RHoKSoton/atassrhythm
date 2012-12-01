cd851ce03043a924a64d39ef1ed3d630031798e0
Thomas Williams <t.r.williamst@googlemail.com>
2012-12-01 13:53:49 +0000

    Packages - started UI

---
+++ RhythmGame/src/com/rhythm/app/MusicRenderer.java
@@ -0,0 +1,74 @@
+package com.rhythm.app;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
+import android.view.SurfaceView;
+
+public class MusicRenderer extends SurfaceView implements Callback 
+{
+	SurfaceHolder holder;
+	Timer tmr = new Timer();
+	
+	public MusicRenderer(Context context) 
+	{
+		super(context);
+		
+		Initialise();
+	}
+
+	public MusicRenderer(Context context, AttributeSet attrs) 
+	{
+		super(context, attrs);
+		
+		Initialise();
+	}
+
+	public MusicRenderer(Context context, AttributeSet attrs, int defStyle) 
+	{
+		super(context, attrs, defStyle);
+		
+		Initialise();
+	}
+
+	private void Initialise()
+	{
+		holder = this.getHolder();
+		holder.addCallback(this);
+	}
+	
+	//Callback methods
+	@Override
+	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) 
+	{
+		
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder arg0) 
+	{
+		TimerTask drawTask = new TimerTask()
+		{
+
+			@Override
+			public void run() 
+			{
+				
+			}
+			
+		};
+		
+		tmr.schedule(drawTask, 0, 16);
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder arg0) 
+	{
+		tmr.cancel();
+	}
+
+}
