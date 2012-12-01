2c2f023ecd2c32e1ef07bf0cbd594235c31187cb
Thomas Williams <t.r.williamst@googlemail.com>
2012-12-01 13:13:03 +0000

    New Android

---
+++ RhythmGame/gen/com/rhythm/app/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.rhythm.app;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- RythmnGame/gen/eyeRhythm/rhythm/app/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package eyeRhythm.rhythm.app;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
